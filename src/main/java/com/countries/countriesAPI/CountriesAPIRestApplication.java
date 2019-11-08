508001bbeea0f6b057c76bc4832e9d33648b2d58
GCPix <gccc0309@gmail.com>
2019-11-08 14:01:33 +0000

    updated userdetail

---
+++ src/main/java/com/countries/countriesAPI/CountriesAPIRestApplication.java
@@ -0,0 +1,13 @@
+package com.countries.countriesAPI;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+@ApplicationScoped
+public class CountriesAPIRestApplication extends Application {
+}
