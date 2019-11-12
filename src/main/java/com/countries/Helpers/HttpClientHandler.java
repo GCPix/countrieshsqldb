1f7b935ca283da9adaa7f7f8c940a2d88d95fd51
GCPix <gccc0309@gmail.com>
2019-11-12 14:15:34 +0000

    added test class for httpClient

--- src/main/java/com/countries/Helpers/HttpClientHandler.java
+++ src/main/java/com/countries/Helpers/HttpClientHandler.java
@@ -1,6 +1,5 @@
 package com.countries.Helpers;
 
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,21 +9,30 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
 
 import com.countries.countriesAPI.Country;
-import com.countries.countriesAPI.Language;
 import com.google.gson.Gson;
 
 public class HttpClientHandler {
+    public String getAPIData(String  url){
+        String jsonString = null;
+         Client client = ClientBuilder.newClient();
+         Response res = client.target(url).request("application/json").get();
+         if (res.getStatus() == 200 && res.hasEntity()){
+            jsonString = res.readEntity(String.class);
+        }
+         return jsonString;
+    }
+
+    public List<Country> jsonToCountry(String jsonString){
+        Gson gson  = new Gson();
+        Country[] countriesArray = gson.fromJson(jsonString, Country[].class);
+        List<Country> countries = new ArrayList<Country>(Arrays.asList(countriesArray));
+        return countries;
+    }    
+    
 
     public static void main(String[] args) {
-        
-        Client client = ClientBuilder.newClient();
-        Response res = client.target("https://restcountries.eu/rest/v2/all").request("application/json").get();
-        
-        String s = res.readEntity(String.class);
-
-        Gson gson  = new Gson();
-        Country[] countriesArray = gson.fromJson(s, Country[].class);
-        List<Country> countries = new ArrayList<Country>(Arrays.asList(countriesArray));
+        HttpClientHandler hpc = new HttpClientHandler();
+        hpc.jsonToCountry(hpc.getAPIData("https://restcountries.eu/rest/v2/all"));
        
     }
    
