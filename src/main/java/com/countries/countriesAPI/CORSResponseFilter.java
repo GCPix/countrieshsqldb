1cc381eaaf8f72526afd60f7a9fe75528151d364
Kortz <hampus.helmbrant.persson@hotmail.se>
2020-01-17 09:33:44 +0000

    Update CORSResponseFilter.java

--- src/main/java/com/countries/countriesAPI/CORSResponseFilter.java
+++ src/main/java/com/countries/countriesAPI/CORSResponseFilter.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
-import javax.ws.rs.container;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.PreMatching;
 
 @Provider
 @PreMatching
@@ -13,7 +16,6 @@ implements ContainerResponseFilter {
 
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
-        Logger.getLogger(CORSResponseFilter.class.getName()).fine("Response intercepted by response filter!");
         responseContext.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
         responseContext.getHeaders().putSingle("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
